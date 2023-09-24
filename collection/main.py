08cbc8183bb542356dab77515f4b030cab30e891
lauren.lowe <lauren.lowe84@gmail.com>
2023-09-23 19:50:48 -0700

    Removed extraneous files and added additional files for setup to include Dockerfiles

---
+++ collection/main.py
@@ -0,0 +1,22 @@
+# import os
+from fastapi import FastAPI
+from routers import inventory
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+app.include_router(inventory.router)
+
+# os.environ.get("CORS_HOST", None),
+origins = [
+    "http://localhost:3000",
+    "CORS_HOST",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
